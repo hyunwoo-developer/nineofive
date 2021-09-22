dd4e989abc425ccab0ad56040e03435bc5707b37
juhwano <calculus9006@gmail.com>
2021-09-22 12:25:05 +0900

    ✨회원 전체 조회 추가

--- nineofive/routes/nine/account/index.js
+++ nineofive/routes/nine/account/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 const accountController = require("../../../controllers/nine/account/accountController");
 var router = express.Router();
 
+router.get("/", accountController.getUser);
 router.post("/signup", accountController.signupUser);
 router.post("/signin", accountController.signinUser);
 
